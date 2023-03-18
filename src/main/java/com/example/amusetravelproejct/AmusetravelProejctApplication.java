24b8c1653e23e8745cf32f1c2d281d0ca6a57a40
hunsuk <daw916@naver.com>
2023-03-18 22:47:21 +0900

    project Init

---
+++ src/main/java/com/example/amusetravelproejct/AmusetravelProejctApplication.java
@@ -0,0 +1,13 @@
+package com.example.amusetravelproejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmusetravelProejctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmusetravelProejctApplication.class, args);
+    }
+
+}
