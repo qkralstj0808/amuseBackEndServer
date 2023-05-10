816f71cd613f21268490386a17ef82b2c67d026f
hunsuk <daw916@naver.com>
2023-05-11 02:10:37 +0900

    [feat] item 생성(티켓제외)

--- src/main/java/com/example/amusetravelproejct/controller/admin/UtilMethod.java
+++ src/main/java/com/example/amusetravelproejct/controller/admin/UtilMethod.java
@@ -1,34 +1,17 @@
-package com.example.amusetravelproejct.controller.admin.service;
+package com.example.amusetravelproejct.controller.admin;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.example.amusetravelproejct.domain.Item;
-import com.example.amusetravelproejct.domain.ItemCourse;
-import com.example.amusetravelproejct.domain.ItemImg;
-import com.example.amusetravelproejct.repository.ItemCourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
-import java.util.Optional;
-
 @AllArgsConstructor
-@Service
-public class ItemCourseService {
-    private final ItemCourseRepository itemCourseRepository;
-    private final AmazonS3 amazonS3Client;
-
+public class UtilMethod {
+    private AmazonS3 amazonS3Client;
     static String bucketName = "amuse-img";
-
-    public Optional<ItemCourse> saveItemCourse(ItemCourse itemCourse) {
-        return Optional.of(itemCourseRepository.save(itemCourse));
-    }
-
-    public String getCourseImgUrl(String base64Img, String key) {
+    public String getCourseImgUrl(String base64Img, String fileName) {
         // TODO
         // 멀티파트, base64로 받은 이미지 S3로 보내어 이미지 저장
         // S3로 보내어 저장한 이미지의 url을 받아와서 List<String>에 저장
@@ -40,7 +23,8 @@ public class ItemCourseService {
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType("image/jpeg");
         metadata.setContentLength(imageBytes.length);
-        amazonS3Client.putObject(bucketName, "images/" + key, inputStream, metadata);
+        String key = "images/" + fileName;
+        amazonS3Client.putObject(bucketName, key, inputStream, metadata);
         String s3Url = amazonS3Client.getUrl(bucketName, key).toString();
 
         return s3Url;
--- src/main/java/com/example/amusetravelproejct/controller/admin/dto/ProductRegisterDto.java
+++ src/main/java/com/example/amusetravelproejct/controller/admin/dto/ProductRegisterDto.java
@@ -14,58 +14,88 @@ public class ProductRegisterDto {
     private String category;
     private String title;
     private Location location;
-    private List<Image> mainImg;
-    private List<Ticket> ticket;
-    private String productInfo;
-    private List<Course> course;
+    private List<MainImageDto> mainImg;
+    private List<TicketDto> ticket;
+    private String mainInfo;
+    private List<CourseDto> course;
     private String extraInfo;
 
+    // Getter, Setter, Constructor 생략
     @Getter
     @Setter
     @NoArgsConstructor
-    static public class Location {
+    @ToString
+    public static class Location {
         private String country;
         private String city;
 
-        //생성자, Getter, Setter 생략
+        // Getters and Setters
     }
     @Getter
     @Setter
     @NoArgsConstructor
-    static public class Image {
+    @ToString
+    public static class MainImageDto {
         private String fileName;
         private String base64Data;
-        //생성자, Getter, Setter 생략
+
+        // Getter, Setter, Constructor 생략
     }
     @Getter
     @Setter
     @NoArgsConstructor
-    static public class Ticket {
+    @ToString
+    public static class TicketDto {
         private String title;
         private String content;
-        private List<Price> priceList;
+        private List<PriceListDto> priceList;
 
-        //생성자, Getter, Setter 생략
+        // Getter, Setter, Constructor 생략
+        @Getter
+        @Setter
+        @NoArgsConstructor
+        @ToString
+        public static class PriceListDto {
+            private String startDate;
+            private String endDate;
+            private String price;
+
+            private WeekdayPrices weekdayPrices;
+            @Getter
+            @Setter
+            @NoArgsConstructor
+            @ToString
+            public static class WeekdayPrices{
+                private String Monday;
+                private String Tuesday;
+                private String Wednesday;
+                private String Thursday;
+                private String Friday;
+            }
+
+            // Getter, Setter, Constructor 생략
+        }
     }
     @Getter
     @Setter
     @NoArgsConstructor
-    static public class Price {
-        private Date startDate;
-        private Date endDate;
-        private String price;
-
-        //생성자, Getter, Setter 생략
-    }
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    static public class Course {
+    @ToString
+    public static class CourseDto {
         private String title;
         private String timeCost;
         private String content;
-        private String imageURL;
+        private CourseImageDto image;
 
-        //생성자, Getter, Setter 생략
+        // Getter, Setter, Constructor 생략
+        @Getter
+        @Setter
+        @NoArgsConstructor
+        @ToString
+        public static class CourseImageDto {
+            private String fileName;
+            private String base64Data;
+
+            // Getter, Setter, Constructor 생략
+        }
     }
 }
