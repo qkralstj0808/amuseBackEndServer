f7648dacc56a8190f7b0c0a20837ac628604acd5
hunsuk <daw916@naver.com>
2023-06-02 21:55:09 +0900

    [fix] 상품 입력 DTO 수정

--- src/main/java/com/example/amusetravelproejct/repository/ItemRepository.java
+++ src/main/java/com/example/amusetravelproejct/repository/ItemRepository.java
@@ -1,7 +1,9 @@
 package com.example.amusetravelproejct.repository;
 
 import com.example.amusetravelproejct.domain.Item;
+import com.example.amusetravelproejct.domain.person_enum.DisplayStatus;
 import com.example.amusetravelproejct.repository.custom.ItemRepositoryCustom;
+import org.springdoc.core.converters.models.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
@@ -13,6 +15,6 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom, QuerydslPredicateExecutor<Item> {
 
     Optional<Item> findByItemCode(String itemCode);
-
+    List<Item> findAllByDisplayStatus(DisplayStatus displayStatus);
 
 }
