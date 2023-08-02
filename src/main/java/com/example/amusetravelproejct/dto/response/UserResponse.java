78411dc9f7c43e6246d32b5bc4dc8d5869007c7d
myhyun0002 <a01089417552@gmail.com>
2023-08-02 16:49:17 +0900

    [feat /user/info

---
+++ src/main/java/com/example/amusetravelproejct/dto/response/UserResponse.java
@@ -0,0 +1,23 @@
+package com.example.amusetravelproejct.dto.response;
+
+import com.example.amusetravelproejct.domain.person_enum.Grade;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class UserResponse {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class getUserInfo {
+        private String id;
+        private String name;
+        private String img_url;
+        private String email;
+        private Grade grade;
+
+    }
+}
