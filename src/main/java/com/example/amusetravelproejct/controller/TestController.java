cd8c5daa93f054745c8426a52346ee435bbbf6ce
myhyun0002 <a01089417552@gmail.com>
2023-09-11 14:18:33 +0900

    BACK-1 [test] jira test

---
+++ src/main/java/com/example/amusetravelproejct/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.amusetravelproejct.controller;
+
+import com.example.amusetravelproejct.config.resTemplate.ResponseTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping("")
+    public ResponseTemplate<String> getAllItemId(){
+
+        return new ResponseTemplate<>("test1");
+    }
+}
