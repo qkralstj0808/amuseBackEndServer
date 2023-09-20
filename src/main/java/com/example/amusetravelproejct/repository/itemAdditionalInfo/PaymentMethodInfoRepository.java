0a6f0f089538d5db579037c5981da7c2c0dfee55
Yoon321 <tkdwls734@gmail.com>
2023-09-20 13:12:36 +0900

    [feat] 추가 정보 관련 레포지토리 생성

---
+++ src/main/java/com/example/amusetravelproejct/repository/itemAdditionalInfo/PaymentMethodInfoRepository.java
@@ -0,0 +1,7 @@
+package com.example.amusetravelproejct.repository.itemAdditionalInfo;
+
+import com.example.amusetravelproejct.domain.itemAdditionalInfo.PaymentMethodInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentMethodInfoRepository extends JpaRepository<PaymentMethodInfo, Long> {
+}
---
+++ src/main/java/com/example/amusetravelproejct/repository/itemAdditionalInfo/TermsOfServiceInoRepository.java
@@ -0,0 +1,7 @@
+package com.example.amusetravelproejct.repository.itemAdditionalInfo;
+
+import com.example.amusetravelproejct.domain.itemAdditionalInfo.TermsOfServiceInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TermsOfServiceInoRepository extends JpaRepository<TermsOfServiceInfo, Long> {
+}
