671cc8e67251491d6ad0b6f6fae36e346d78e85b
codekim97 <kimtaeu4616@gmail.com>
2023-09-27 15:05:36 +0900

    1. Payment 관련 테이블 수정

--- src/main/java/com/example/amusetravelproejct/domain/payment/MainPayment.java
+++ src/main/java/com/example/amusetravelproejct/domain/payment/MainPayment.java
@@ -6,32 +6,40 @@ import com.example.amusetravelproejct.domain.User;
 import com.example.amusetravelproejct.domain.person_enum.PayStatus;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity(name = "mainPayment")
 @EntityListeners(value = {AuditingEntityListener.class})
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class MainPayment extends BaseEntity {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String itemName;
     private String itemImage;
 
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate travelStartDate; // 여행 시작 날짜
+
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate travelEndDate; // 여행 종료 날짜
 
     @Enumerated(value = EnumType.STRING)
     private PayStatus payStatus; // 결제 상태 SUCCESS, CANCEL, PENDING
 
-    private Integer itemPayCost; // 결제 당시 상품의 결제 가격
+    private Integer itemPayPrice; // 결제 당시 상품의 결제 가격
 
-    private LocalDate reservationDate; // 결제(예약)날짜
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Builder.Default
+    private LocalDateTime reservationDateTime = LocalDateTime.now(); // 결제(예약)날짜
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
--- src/main/java/com/example/amusetravelproejct/dto/request/payment/PaymentPageRequestDto.java
+++ src/main/java/com/example/amusetravelproejct/dto/request/payment/PaymentPageRequestDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+//@AllArgsConstructor
 @NoArgsConstructor
 public class PaymentPageRequestDto {
 }
